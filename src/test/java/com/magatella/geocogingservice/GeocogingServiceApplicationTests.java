5a6ac073979e98173fdd7b7d286d06b1fb3398af
Magomed Muradkhanov <magamuradkhanov@gmail.com>
2022-12-09 00:01:38 +0300

    Init commit

---
+++ src/test/java/com/magatella/geocogingservice/GeocogingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magatella.geocogingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocogingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
